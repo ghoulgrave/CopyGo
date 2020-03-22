607b97956035e2a75ab825524c969c5e97903d29
ghoulgrave <ghoulgrave@163.com>
2020-03-22 15:36:15 +0800

    测试

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/leaanthony/mewn v0.10.7
 	github.com/wailsapp/wails v1.0.2
+	github.com/spf13/viper v1.6.2
 )
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/leaanthony/mewn"
+	"github.com/spf13/viper"
 	"github.com/wailsapp/wails"
 	"os"
 	"path"
@@ -12,6 +13,9 @@ func basic() string {
 	return "Hello World!"
 }
 
+//我的配置文件
+var MyConfig Config
+
 func main() {
 
 	js := mewn.String("./frontend/dist/app.js")
@@ -19,8 +23,29 @@ func main() {
 
 	ePath, _ := os.Executable()
 	fmt.Println(ePath)
-	fmt.Println(path.Dir(ePath))
+	conpath := path.Dir(ePath)
 
+	viper.SetConfigName("copy")             // 设置配置文件名 (不带后缀)
+	viper.AddConfigPath(conpath + "/conf/") // 第一个搜索路径
+	viper.SetConfigType("yaml")
+
+	err := viper.ReadInConfig() // 读取配置数据
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+	viper.Unmarshal(&MyConfig) // 将配置信息绑定到结构体上
+
+	//更新配置文件
+	//fmt.Println(MyConfig)
+	v := MyConfig.Conf
+	k := Confs{Sub_path: "http://ssss", Name: "ttttt", Dir_path: "/user/dfds/sdfsfd", Out_path: "/sss.dd/fffs/affd"}
+	v = append(v, k)
+	viper.Set("conf", v)
+
+	viper.Unmarshal(&MyConfig)
+	//fmt.Println(MyConfig)
+
+	viper.WriteConfig()
 	app := wails.CreateApp(&wails.AppConfig{
 		Width:  1024,
 		Height: 768,
@@ -56,9 +81,42 @@ func datas() string {
 	name: 'zhangyiyang4',
 	address: '/ddd/dd4444'
 }
-
-
-
-
 ]`
+}
+
+type SvnInfo struct {
+	Name    string `json:"name"`
+	Time    string `json:"time"`
+	Version string `json:"version"`
+	Path    string `json:"path"`
+	SubLogs string `json:"sublogs"`
+}
+
+type MyData struct {
+	Code  int64     `json:"code"`
+	Msg   string    `json:"msg"`
+	Count int64     `json:"count"`
+	Data  []SvnInfo `json:"data"`
+	Other int64     `json:"-"` // 直接忽略字段
+}
+
+type Search struct {
+	ConfigNum string
+	Kssj      string
+	Jssj      string
+	Name      string
+	Build     string
+}
+
+type Config struct {
+	Username string
+	Conf     []Confs
+}
+
+type Confs struct {
+	Sub_path string
+	Dir_path string
+	Svn_path string
+	Out_path string
+	Name     string
 }
